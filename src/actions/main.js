ecbaf224efd368bbbb154a49d2084906a1e5d67c
Tolokoban <contact@tolokoban.org>
2016-04-14 13:28:16 +0200

    Update main.js

--- src/actions/main.js
+++ src/actions/main.js
@@ -1,4 +1,10 @@
 ["demo", [
+    ["reset", { 
+        today: 20160501,
+        firstname: "Homer",
+        lastname: "Broken"
+    }],
     ["text", "Welcome! Please select a scenario:"],
-    ["button", { text: "Scenario blablabla", action: "scenario1" }]
+    ["button", { text: "Mr {{lastname}} limbs a bit", action: "scenario1" }],
+    ["button", { text: "Miss <i>blabla</i> has <i>blabla</i>", action: "scenario2" }],
 ]]
