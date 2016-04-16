43ae824aa77bfa76fdaaabae401cf1579ad9a755
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 19:02:10 +0200

    Delete 03-radiology-blood-appointment.js

--- src/actions/story1/03-radiology-blood-appointment.js
+++
@@ -1,8 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:04-radiology-blood"
-    }],
-    ['text', "Patient took a radiology appointment"],
-    ['text', "Patient took a blood test appointment"],
- ['button', { text: 'App', action: "app:start" }]
-]]
