513fff1b93dafad5664752759d9f72f4ed8f60e5
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 16:53:56 +0200

    Update surgeon-first-meeting.js

--- src/actions/story1/surgeon-first-meeting.js
+++ src/actions/story1/surgeon-first-meeting.js
@@ -1,6 +1,6 @@
 ['story', [
     ["set", {
-        $next: "prescriptions-appointments"
+        $next: "story1:prescriptions-appointments"
     }],
     ['text', "Surgeon provides consultation services and prescribes radio., blood..."],
     ['button', { text: "APP", action: "app:start" }]
