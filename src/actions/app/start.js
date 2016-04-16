105ce9b265c71cc9311cc7e535263b22edb4e057
Tolokoban <contact@tolokoban.org>
2016-04-16 18:02:44 +0200

    Next

--- src/actions/app/start.js
+++ src/actions/app/start.js
@@ -1,6 +1,7 @@
 ['app', [
     ['nurse', "How can I help you {{dg.firstname}} {{dg.lastname}}?"],
     ['button', { text: "I took an appointment.", action: "appointments" }],
+    ['button', { text: "Edit demographic.", action: "demographic" }],
     ['button', { text: "I want to send my data.", action: "" }],
     ['button', { text: "I want to add data.", action: "" }]
 ]]
