3e9eeaa65083c04c05ff22a507cad88e7a10ddef
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:17:40 +0200

    Delete intervention-appointment.js

--- src/actions/story1/intervention-appointment.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:anaesthetist-appointment"
-    }],
-    ['text', "Patient takes the appointment for the intervention "],
- ['button', { text: 'App', action: "app:start" }],
-]]
