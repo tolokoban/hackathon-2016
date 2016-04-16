b0a17f0247c0770bd31490e0ebc485b78ee98241
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 15:49:37 +0200

    Create diagnosis-appointment.js

---
+++ src/actions/story1/diagnosis-appointment.js
@@ -0,0 +1,7 @@
+['story', [
+    ["set", {
+        $next: "physiotherapy-appointment"
+    }],
+    ['text', "Surgeon provides the diagnosis and prescribes the intervention, the treatment and the physiotherapy"]],
+ ['button', { text: 'App', action: "app:start" }]
+]]
