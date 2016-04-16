ecc281e06bf008a3a5e6f23d5e7fa976aca69a56
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:11:53 +0200

    Delete physiotherapy-appointment.js

--- src/actions/story1/physiotherapy-appointment.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:physiotherapy-meeting"
-    }],
-    ['text', "Patient takes the appointment for the consultation with the physiotherapist "],
- ['button', { text: 'App', action: "app:start" }]
-]]
