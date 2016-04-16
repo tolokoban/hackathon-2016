9e64cf2b48b5ec2d9471a2b187a77ccb7f885d07
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:17:16 +0200

    Update and rename physiotherapy-meeting.js to 06-physiotherapy-meeting.js

---
+++ src/actions/story1/06-physiotherapy-meeting.js
@@ -0,0 +1,7 @@
+['story', [
+    ["set", {
+        $next: "story1:07-intervention-appointment"
+    }],
+    ['text', "Physiotherapist prescribed the pre/post-op therapy <br/> Patient took the surgery appointment"],
+ ['button', { text: 'App', action: "app:start" }]
+]]
--- src/actions/story1/physiotherapy-meeting.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:intervention-appointment"
-    }],
-    ['text', "Physiotherapist proposes the therapy "],
- ['button', { text: 'App', action: "app:start" }]
-]]
