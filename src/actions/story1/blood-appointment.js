6a13a7912b933be2111d13e355bd0b7dc1f84c98
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 16:29:12 +0200

    Update blood-appointment.js

--- src/actions/story1/blood-appointment.js
+++ src/actions/story1/blood-appointment.js
@@ -1,6 +1,6 @@
 ['story', [
     ["set", {
-        $next: "diagnosis-appointment"
+        $next: "diagnosis-meeting"
     }],
     ['text', "Patient takes an appointment for blood tests"],
  ['button', { text: 'App', action: "app:start" }]
