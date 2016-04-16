992d18d4267e87c2e45029d819100f99cab28242
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 16:50:52 +0200

    Update radiology-appointment.js

--- src/actions/story1/radiology-appointment.js
+++ src/actions/story1/radiology-appointment.js
@@ -1,6 +1,6 @@
 ['story', [
     ["set", {
-        $next: "blood-appointment"
+        $next: "story1:blood-appointment"
     }],
     ['text', "Patient takes a radiology appointment"],
  ['button', { text: 'App', action: "app:start" }]
