1749064d5cb2afbbc81021ad92adb595c7dcdf85
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:30:00 +0200

    Update and rename pre-surgery-anaesthetist-consultation.js to 08-pre-surgery-anaesthetist-consultation.js

---
+++ src/actions/story1/08-pre-surgery-anaesthetist-consultation.js
@@ -0,0 +1,7 @@
+['story', [
+    ["set", {
+        $next: ""
+    }],
+    ['text', "Patient had pre surgery consultation with the anaesthetist "],
+ ['button', { text: 'App', action: "app:start" }],
+]]
--- src/actions/story1/pre-surgery-anaesthetist-consultation.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:pre-admission-testing"
-    }],
-    ['text', " Pre surgery consultation with the anaesthetist "],
- ['button', { text: 'App', action: "app:start" }],
-]]
