a4ba9770f8e7f086490925cb78e1ade7eef128d9
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:11:34 +0200

    Update and rename 04-diagnosis-meeting.js to 05-diagnosis-meeting.js

--- src/actions/story1/04-diagnosis-meeting.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:physiotherapy-appointment"
-    }],
-    ['text', "Surgeon provides the diagnosis and prescribes the intervention, the treatment and the physiotherapy"],
- ['button', { text: 'App', action: "app:start" }]
-]]
---
+++ src/actions/story1/05-diagnosis-meeting.js
@@ -0,0 +1,7 @@
+['story', [
+    ["set", {
+        $next: "story1:06-physiotherapy-meeting"
+    }],
+    ['text', "Surgeon provides the diagnosis and prescribes the intervention, the treatment and the physiotherapy <br/> Patient took the appointment for consultation with physiotherapist "],
+ ['button', { text: 'App', action: "app:start" }]
+]]
