a8dbd86fcf58be27dc2b7f5c27ec07a36e79abf2
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:03:24 +0200

    Update and rename radiology-appointment.js to 03-radiology-blood-appointment.js

--- src/actions/story1/radiology-appointment.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:blood-appointment"
-    }],
-    ['text', "Patient takes a radiology appointment"],
- ['button', { text: 'App', action: "app:start" }]
-]]
