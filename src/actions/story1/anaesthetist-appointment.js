c8f23ca18e5e5c7f0beae5f04b3d97e76d9a6c55
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:19:00 +0200

    Update and rename anaesthetist-appointment.js to 07-anaesthetist-appointment.js

--- src/actions/story1/anaesthetist-appointment.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:pre-surgery-anaesthetist-consultation"
-    }],
-    ['text', "Patient waits for the anaesthetist to propose the date and time for the consultation appointment  "],
- ['button', { text: 'App', action: "app:start" }],
-]]
