53ddb374c5ec390784243dfd7d93758299671587
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:30:26 +0200

    Delete admission.js

--- src/actions/story1/admission.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:check-in-surgery"
-    }],
-    ['text', " Prepare for the admission (for patient service) "],
- ['button', { text: 'App', action: "app:start" }],
-]]
