f12abfdabd4a03aabd92e0a3a2d12b8390e581fa
Tolokoban <contact@tolokoban.org>
2016-04-16 14:36:09 +0200

    .

---
+++ src/actions/story1/start.js
@@ -0,0 +1,4 @@
+['story', [
+    ['text', "Patient feels pain.<br/> Patient takes an appointment wih GP.<br/> PAtient takes appt with surgeaon."]],
+ ['button', { text: 'App', action: "app:start" }]
+]]
