ae959f1e202b006c3d469de46341b82c8242030c
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 15:58:43 +0200

    Update physiotherapy-appointment.js

--- src/actions/story1/physiotherapy-appointment.js
+++ src/actions/story1/physiotherapy-appointment.js
@@ -2,6 +2,6 @@
     ["set", {
         $next: "physiotherapy-meeting"
     }],
-    ['text', ""]],
+    ['text', "Patient takes the appointment for the consultation with the physiotherapist "],
  ['button', { text: 'App', action: "app:start" }]
 ]]
