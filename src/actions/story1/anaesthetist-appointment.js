9b72b54d7589ac8523d30618a236a457af614a69
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 16:51:22 +0200

    Update anaesthetist-appointment.js

--- src/actions/story1/anaesthetist-appointment.js
+++ src/actions/story1/anaesthetist-appointment.js
@@ -1,6 +1,6 @@
 ['story', [
     ["set", {
-        $next: "pre-surgery-anaesthetist-consultation"
+        $next: "story1:pre-surgery-anaesthetist-consultation"
     }],
     ['text', "Patient waits for the anaesthetist to propose the date and time for the consultation appointment  "],
  ['button', { text: 'App', action: "app:start" }],
