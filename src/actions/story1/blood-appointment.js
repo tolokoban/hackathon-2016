2f1fc82acd745ff93a1576db27f586864ceab527
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:01:16 +0200

    Delete blood-appointment.js

--- src/actions/story1/blood-appointment.js
+++
@@ -1,7 +0,0 @@
-['story', [
-    ["set", {
-        $next: "story1:diagnosis-meeting"
-    }],
-    ['text', "Patient takes an appointment for blood tests"],
- ['button', { text: 'App', action: "app:start" }]
-]]
