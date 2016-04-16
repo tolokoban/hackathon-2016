acf2e05e60f8d01beb28a4ac3ac30c0c4d36935f
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 16:00:36 +0200

    Create physiotherapy-meeting.js

---
+++ src/actions/story1/physiotherapy-meeting.js
@@ -0,0 +1,7 @@
+['story', [
+    ["set", {
+        $next: "intervention-appointment"
+    }],
+    ['text', "Physiotherapist proposes the therapy "],
+ ['button', { text: 'App', action: "app:start" }]
+]]
