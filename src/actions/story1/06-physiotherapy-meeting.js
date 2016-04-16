bc499ee5deaed8b1eedf810d1f0cc9733667d62f
DalilaSalamani <ad_salamani@esi.dz>
2016-04-16 18:54:14 +0200

    Update 06-physiotherapy-meeting.js

--- src/actions/story1/06-physiotherapy-meeting.js
+++ src/actions/story1/06-physiotherapy-meeting.js
@@ -1,6 +1,6 @@
 ['story', [
     ["set", {
-        $next: "story1:07-intervention-appointment"
+        $next: "story1:07-anaesthetist-appointment"
     }],
     ['text', "Physiotherapist prescribed the pre/post-op therapy <br/> Patient took the surgery appointment"],
  ['button', { text: 'App', action: "app:start" }]
